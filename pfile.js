c4d208c7c12e9a944faba4118a59f410474d6979
PabloCloudX <lilikfarida067@gmail.com>
2025-12-17 13:02:10 +0700

    Update pfile.js

--- pfile.js
+++ pfile.js
@@ -1,17 +1,38 @@
 const path = require('path')
 const fs = require('fs')
 
-function pfile(relPath) {
-  if (!relPath.startsWith('./')) {
-    return require(relPath)
+const DEFAULT_EXTENSIONS = ['.js', '.json', '.node']
+
+function resolveFile(relPath) {
+  const absPath = path.isAbsolute(relPath)
+    ? relPath
+    : path.resolve(process.cwd(), relPath)
+
+  if (fs.existsSync(absPath) && fs.statSync(absPath).isDirectory()) {
+    return absPath
   }
-    
-  const absPath = path.join(process.cwd(), relPath)
-  return require(absPath)
+
+  if (fs.existsSync(absPath) && fs.statSync(absPath).isFile()) {
+    return absPath
+  }
+
+  for (const ext of DEFAULT_EXTENSIONS) {
+    const tryPath = absPath + ext
+    if (fs.existsSync(tryPath) && fs.statSync(tryPath).isFile()) {
+      return tryPath
+    }
+  }
+
+  throw new Error(`Path not found: ${relPath}`)
+}
+
+function pfile(relPath) {
+  const filePath = resolveFile(relPath)
+  return require(filePath)
 }
 
 function ppath(relPath) {
-  return path.resolve(process.cwd(), relPath)
+  return resolveFile(relPath)
 }
 
 module.exports = { pfile, ppath }
